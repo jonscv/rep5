5d9b36174ab59e6cfb9f997dcec5b6cd838420ce
users <businessitspecialist@googlemail.com>
2017-04-06 16:01:49 +0100

    Revert "baseline_0"
    This reverts commit c4a3ffc7d3cb5e150b06011b98816141318fa9b6.
    
    # Conflicts:
    #	app/assets/javascripts/application.js
    #	app/assets/javascripts/matches.coffee
    #	app/assets/stylesheets/application.css
    #	app/views/matches/index.html.erb
    #	app/views/matches/show.html.erb

--- db/migrate/20170406100120_add_age_to_matches.rb
+++
@@ -1,5 +0,0 @@
-class AddAgeToMatches < ActiveRecord::Migration[5.0]
-  def change
-    add_column :matches, :Age, :integer
-  end
-end
--- db/migrate/20170406124727_add_sex_to_matches.rb
+++
@@ -1,5 +0,0 @@
-class AddSexToMatches < ActiveRecord::Migration[5.0]
-  def change
-    add_column :matches, :Sex, :string
-  end
-end
